e33bce35cc0e098e821e903ebbde821164ac2deb
jeSoosRemirez <dkhomych0091@gmail.com>
2022-01-20 21:55:58 +0200

    Add todo_db to DATABASES

--- todo_proj/todo_proj/settings.py
+++ todo_proj/todo_proj/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'todo_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',    # Default HOST: localhost
+        'PORT': 5432,   # Default PORT: 5432
     }
 }
 
