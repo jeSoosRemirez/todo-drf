3a9baa2087e8d80d8a72d26b8124bb5c77d74efc
jeSoosRemirez <dkhomych0091@gmail.com>
2022-01-23 19:35:03 +0200

    Create model for task

--- todo_proj/todo_app/models.py
+++ todo_proj/todo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from users.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    owner = models.ForeignKey(User, related_name='owner', on_delete=models.CASCADE)
+    header = models.TextField(max_length=50, blank=False)
+    text = models.TextField(max_length=100, blank=False)
+    liable = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(verbose_name='image', upload_to='media/', blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_time']
