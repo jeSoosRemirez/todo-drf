b5e3abc6d23f5d9da1f17d233fd858c93e982d3b
jeSoosRemirez <dkhomych0091@gmail.com>
2022-01-23 19:57:35 +0200

    Create model for user

--- todo_proj/users/models.py
+++ todo_proj/users/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
+)
+import jwt
+from django.conf import settings
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves the User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class that implements a fully featured User model with
+    admin-compliant permissions.
+    """
+
+    email = models.EmailField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    token_key = models.CharField(max_length=512, default="")
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
+
+    # user.token will give you a token
+    @property
+    def token(self):
+        return self._generate_jwt_token()
+
+    # Generates a JSON with user id and token
+    def _generate_jwt_token(self):
+        dt = datetime.now() + timedelta(days=1)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token.decode('utf-8')
