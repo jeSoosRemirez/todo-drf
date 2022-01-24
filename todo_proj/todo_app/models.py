57b17b632132390b6ca2a0a80938b89c97bdf452
jeSoosRemirez <dkhomych0091@gmail.com>
2022-01-24 13:00:23 +0200

    Add edited_time field and functions to it

--- todo_proj/todo_app/models.py
+++ todo_proj/todo_app/models.py
@@ -1,5 +1,5 @@
+from django.utils import timezone
 from django.db import models
-from django.conf import settings
 from users.models import User
 
 
@@ -10,6 +10,16 @@ class Task(models.Model):
     liable = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     image = models.ImageField(verbose_name='image', upload_to='media/', blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
+    edited_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['created_time']
+
+    def __init__(self, *args, **kwargs):
+        super(Task, self).__init__(*args, **kwargs)
+        self.created_time = self.created_time
+
+    def save(self, *args, **kwargs):
+        if not self.created_time and self.created_time:
+            self.edited_date = timezone.now()
+        super(Task, self).save(*args, **kwargs)
